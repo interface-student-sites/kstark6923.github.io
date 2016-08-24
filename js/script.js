ee7b6ad8b0dd3fa7c539671b0fb22b780f0dbe22
kstark6923 <kellysstark@yahoo.com>
2016-08-23 19:52:04 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('p').fadeIn(2000);
     
     $('header').click(function(){
-        $('header').css("background-color", "red");
+        $('this').css("background-color", "red");
     });
 });
 
